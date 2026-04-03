cd161f59c2ed56171ffa578d79a284af192f5ab8
gamingyuvraj551-wq <gamingyuvraj551@gmail.com>
2026-04-03 13:26:12 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
+const CACHE_NAME = 'moms-kitchen-v2'; // Ise v2 kar diya hai taaki refresh ho jaye
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './sw.js'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch resources
 self.addEventListener('fetch', (event) => {
-  // Ye line app ko installable banati hai
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
